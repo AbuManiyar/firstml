12efab9431770578ebdfbb3741955c46b1f22e7d
Abutalha <maniyarabutalha00@gmail.com>
2023-10-29 16:56:41 +0530

    initialized

---
+++ setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+
+
+def get_requirements(file_path:str)->List[str]:
+    requirements = []
+    with open(file_path) as file:
+        requirements = file.readlines()
+        requirements = [i.replace('\n', '') for i in requirements]
+        if '-e .' in requirements:
+            requirements.remove('-e .')
+            
+    return requirements
+
+
+setup(
+    name='first',
+    version= '0.0.1',
+    author= 'abutalha',
+    author_email= 'maniyarabutalha00@gmail.com',
+    packages = find_packages(),
+    install_requires = get_requirements('requirements.txt')
+    )
